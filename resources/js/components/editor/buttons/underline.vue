efd2b0be6e9f0a54260b6ab55a8f7622303ec1d7
Yi Yang <yi@fanswoo.com>
2022-08-17 04:54:11 +0800

    add editor underline, placeholder

---
+++ resources/js/components/editor/buttons/underline.vue
@@ -0,0 +1,14 @@
+<template>
+  <button
+    :class="{ 'is-active': editor.isActive('underline') }"
+    @click="editor.chain().focus().toggleUnderline().run()"
+  >
+    <FontAwesomeIcon icon="underline" />
+  </button>
+</template>
+
+<script>
+export default {
+  inject: ['editor'],
+};
+</script>
